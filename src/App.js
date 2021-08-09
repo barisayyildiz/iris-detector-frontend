27b07ad6ee5a07c8c4c3a3390c2aa0f8156237b7
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-08-09 17:41:47 +0300

    backend e bağlandı

--- src/App.js
+++ src/App.js
@@ -1,41 +1,78 @@
 import './App.scss';
 import RangeSlider from './components/RangeSlider'
+import { useState, useRef } from 'react';
+import axios from 'axios';
 
 function App() {
+
+	const containerRef = useRef(null);
+	const [response, setResponse] = useState(null);
+	const [url, setUrl] = useState(null);
+
+	const makeRequest = () => {
+		const array = containerRef.current.children
+		const body = {
+			"sepal_width" : parseFloat(array[0].firstElementChild.value),
+			"sepal_length" : parseFloat(array[1].firstElementChild.value),
+			"petal_width" : parseFloat(array[2].firstElementChild.value),
+			"petal_length" : parseFloat(array[3].firstElementChild.value)
+		}
+
+		console.log(body)
+
+		axios.post('http://127.0.0.1:8000/api/hello/', {
+			...body
+		}).then(res => {
+
+			console.log(res)
+
+			const { name, url } = res.data.result;
+			setResponse(name);
+			setUrl(url);
+
+			// const { name, url } = res.data;
+			// setResponse(name);
+			// setUrl(url);
+			// console.log(name, url)
+		})
+
+	}
+
   return (
     <div className="App">
-			
 			<h1 className="header">Iris Flower Detector</h1>
-
-			<div className="slider-container">
-				
+			<div className="slider-container" ref={containerRef}>
 				<RangeSlider
-					min={100}
-					max={200}
+					min={0}
+					max={10}
 					text={"Sepal Width"}
 				></RangeSlider>
-
 				<RangeSlider
-					min={100}
-					max={200}
+					min={0}
+					max={10}
 					text={"Sepal Length"}
 				></RangeSlider>
-
 				<RangeSlider
-					min={100}
-					max={200}
+					min={0}
+					max={10}
 					text={"Petal Width"}
 				></RangeSlider>
-
 				<RangeSlider
-					min={100}
-					max={200}
+					min={0}
+					max={10}
 					text={"Petal Length"}
 				></RangeSlider>
-
 			</div>
+			<button id="detect" onClick={makeRequest}>Detect</button>
 
-			<button id="detect">Detect</button>
+			{
+				response && (
+						<>
+							<h3>Your flower name is {response}</h3>
+							<img className="flower_image" stlye={{width:'10px'}} src={url}></img>
+						</>
+					)
+			}
 
     </div>
   );
--- src/components/RangeSlider/index.js
+++ src/components/RangeSlider/index.js
@@ -7,13 +7,10 @@ function RangeSlider({min=0, max=500, initial=(min+max)/2, text}) {
 	const [value, setValue] = useState(initial);
 	
 	const rangeSliderChange = () => {
-		setValue(parseInt(rangeRef.current.value))
+		setValue(rangeRef.current.value)
 	}
 
-
 	const percentage = ((value-min) / (max-min)) * 100;
-	console.log(percentage)
-
 	const labelStyle={
 		position:"absolute",
 		left: `calc(${percentage}%)`,
@@ -21,9 +18,9 @@ function RangeSlider({min=0, max=500, initial=(min+max)/2, text}) {
 	}
 
 	return (
-		<div class="range-slider-wrapper">
-			<input  class="range-slider" ref={rangeRef} type="range" value={value} min={min} max={max} onChange={rangeSliderChange}></input>
-			<span style={labelStyle}  class="range-slider-value">{value}</span>
+		<div className="range-slider-wrapper">
+			<input  className="range-slider" ref={rangeRef} type="range" step={0.1} value={value} min={min} max={max} onChange={rangeSliderChange}></input>
+			<span style={labelStyle}  className="range-slider-value">{value}</span>
 			<p>{text} (cm)</p>
 		</div>
 	)
